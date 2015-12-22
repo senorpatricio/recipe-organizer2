1af9d4292b2a0cecc896847a7373c1deeeae50a2
Patrick Harding <patrickharding@patrickharding.local>
2015-12-22 10:35:39 -0700

    updated styles, added background, getting ready to add ingredients

--- backend/apps/recipes/models.py
+++ backend/apps/recipes/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+
+# class Ingredient(models.Model):
+#     name = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.name
+
+
 class Recipe(models.Model):
 	name = models.CharField(max_length=100)
 	directions = models.TextField()
 	description = models.TextField()
+	# ingredients = models.ManyToManyField(Ingredient)
 
 	def __str__(self):
 		return self.name 
--- backend/apps/recipes/serializers.py
+++ backend/apps/recipes/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from models import *
 
+# class IngredientSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Ingredient
+
 class RecipeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Recipe
+
